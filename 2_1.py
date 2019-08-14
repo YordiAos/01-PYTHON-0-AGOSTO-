53b83bc85544a75610d95279f112d83b865028ce
Yordinn jojojo <yonosemanana@example.com>
2019-08-13 20:08:38 -0500

    hasta video 2.2

---
+++ 2_1.py
@@ -0,0 +1,3 @@
+#2.1 Recibiendo valores por consola(EDTEAM PYTHON basico)
+dato=int(input('Ingresa un numero:'))
+print(dato)
---
+++ 2_2.py
@@ -0,0 +1,5 @@
+#2.1 Recibiendo valores por consola(EDTEAM PYTHON basico)
+dato_1=int(input('Ingresa  numero 1:'))
+dato_2=int(input('Ingresa  numero 2: '))
+print(dato_1,dato_2)
+if
