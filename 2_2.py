cacc0152b39951beea6056ddeae88503cbad7f8d
Yordinn jojojo <yonosemanana@example.com>
2019-08-14 21:21:17 -0500

    fin video 2.4

--- 2_2.py
+++ 2_2.py
@@ -1,13 +1,22 @@
 #2.2 condicional if
+#2.3 IDENTACION
+#2.4 Condicional if anidado
 dato_1=int(input('Ingresa  numero 1:'))
 dato_2=int(input('Ingresa  numero 2: '))
+dato_3=int(input('Ingresa  numero 2: '))
 cadena='holassss'
 #print(dato_1+dato_2,cadena)
 #print(dato_1,dato_2,cadena)
 """comment multilinea"""
 
 if dato_1>dato_2:
-    print(dato_1)
-#2.3 IDENTACION
-#2.4 Condicional if anidado
+    if dato_1>dato_3:
+        print('El mayor es: ',dato_1)
+    else:
+        print('el mayor es: ',dato_3)
+else:
+    if dato_2>dato_3:
+        print('El mayor es: ',dato_2)
+    else:
+        print('El mayor es:',dato_3)
     
