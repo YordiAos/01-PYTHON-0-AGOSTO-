32bc746c60e3bb05c0d90bc2c40620430995c88e
Yordinn jojojo <yonosemanana@example.com>
2019-08-14 21:48:04 -0500

    fin v 2.5

--- 2_2.py
+++ 2_2.py
@@ -21,14 +21,39 @@ else:
         print('El mayor es:',dato_3)
 '''
 #REALIZANDO LO MISMO con menos lineas
-if dato_1>dato_2 and dato_1>dato_3 and True:#pude haber puesto un booleano
+
+#igual igual igual en python
+if dato_1==dato_2==dato_3:
+    print('todos son iguales')
+    
+#tenemos 3 casos
+#1 caso
+elif dato_1==dato_2:
+    print('Existe 2 valores que son iguales')
+    if dato_1>dato_3:
+        print ('Los mayores son: ',dato_1,dato_2)#podria poner cualquiera
+    else:
+        print ('el mayor es: ',dato_3)
+#2 caso
+elif dato_2==dato_3:
+    print('Existe 2 valores que son iguales')
+    if dato_2>dato_1:
+        print ('Los mayores son: ',dato_2,dato_2)#podria poner cualquiera
+    else:
+        print ('el mayor es: ',dato_1)
+# 3 caso    
+elif dato_1==dato_3:
+    print('Existe 2 valores que son iguales')
+    if dato_1>dato_2:
+        print ('Los mayores son: ',dato_1,dato_1)#podria poner cualquiera
+    else:
+        print ('el mayor es: ',dato_2)
+        
+elif dato_1>dato_2 and dato_1>dato_3 and True:#pude haber puesto un booleano
     print('El mayor es: ',dato_1)
 elif dato_2>dato_1 and dato_2>dato_3:
     print('El mayor es: ',dato_2)
 else:
     print('El mayor es: ',dato_3)
-#igual igual igual en python
-if dato_1==dato_2==dato_3:
-    print('todos son iguales')
-else:
-    print('Existen valores q no son iguales')
+#else:
+ #   print('Existen valores q no son iguales')
