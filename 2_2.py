201ee4760fe35aef556b54e4dde7b95d47bb9034
Yordinn jojojo <yonosemanana@example.com>
2019-08-14 21:15:03 -0500

    fin video 2.3

--- 2_2.py
+++ 2_2.py
@@ -1,5 +1,13 @@
-#2.1 Recibiendo valores por consola(EDTEAM PYTHON basico)
+#2.2 condicional if
 dato_1=int(input('Ingresa  numero 1:'))
 dato_2=int(input('Ingresa  numero 2: '))
-print(dato_1,dato_2)
-if
+cadena='holassss'
+#print(dato_1+dato_2,cadena)
+#print(dato_1,dato_2,cadena)
+"""comment multilinea"""
+
+if dato_1>dato_2:
+    print(dato_1)
+#2.3 IDENTACION
+#2.4 Condicional if anidado
+    
