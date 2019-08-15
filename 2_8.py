884f9d78849fb70238348289a9e645b6741c30fc
Yordinn jojojo <yonosemanana@example.com>
2019-08-14 22:15:51 -0500

    fin v2.8 funciones

---
+++ 2_8.py
@@ -0,0 +1,14 @@
+def saludo():
+    print('hola mundo')
+'''
+saludo()
+saludo()
+'''
+def cuadrado(n):
+    print(n*n)
+cuadrado(10)
+cuadrado(7)
+
+def cuadrado_op3(n):
+    return n*n
+print(cuadrado_op3(3))
