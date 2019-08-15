e276670f0080d2fab6d5d303024010ded46ba341
Yordinn jojojo <yonosemanana@example.com>
2019-08-14 22:01:33 -0500

    fin v 2,7

---
+++ 2_7.py
@@ -0,0 +1,16 @@
+'''
+x=20
+while x>=10:
+    print (x)
+    x=x-1
+'''
+#imprimiendo de -10 a 10
+for x in range(-10,11,2):
+    print(x)
+datos=['dato1',['dato2']],['dato3']
+for dato in datos:
+    print(dato)
+#imprimiendo cadenas
+palabra='holap'
+for letra in palabra:
+    print(letra)
